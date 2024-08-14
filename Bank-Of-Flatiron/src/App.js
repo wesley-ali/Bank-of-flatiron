b89c2704ef59a6cfff34a889ca73d9ac242c220d
Wesley Alinyo <wesleyalinyo@gmail.com>
2024-08-14 15:04:14 +0300

    Modifies App.css file

--- Bank-Of-Flatiron/src/App.js
+++ Bank-Of-Flatiron/src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:8001/transactions')
+    fetch('http://localhost:3000/transactions')
       .then(response => response.json())
       .then(data => setTransactions(data));
   }, []);
