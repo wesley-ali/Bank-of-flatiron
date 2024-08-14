48749a7df2dfb75be2acd9cf8e52238188525f74
Wesley Alinyo <wesleyalinyo@gmail.com>
2024-08-14 14:31:46 +0300

    Adds an App.js file

---
+++ Bank-Of-Flatiron/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import TransactionTable from './TransactionTable';
+import TransactionForm from './TransactionForm';
+import SearchBar from './SearchBar';
+import './App.css';
+
+function App() {
+  const [transactions, setTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:8001/transactions')
+      .then(response => response.json())
+      .then(data => setTransactions(data));
+  }, []);
+
+  const addTransaction = (transaction) => {
+    setTransactions([...transactions, transaction]);
+  };
+
+  const filteredTransactions = transactions.filter(transaction =>
+    transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="App">
+      <h1>Bank of FlatIron</h1>
+      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <TransactionForm addTransaction={addTransaction} />
+      <TransactionTable transactions={filteredTransactions} />
+    </div>
+  );
+}
+
+export default App;
